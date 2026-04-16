fbc89800f6211c3e45010224e22172d559677808
Nathaniel Cabral <nathaniel.cabral44@gmail.com>
2026-04-15 20:43:28 -0700

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, render_template_string
 import os
-import sys
 from model_utils import load_pipeline, predict_next_item
 from dotenv import load_dotenv
 load_dotenv()
